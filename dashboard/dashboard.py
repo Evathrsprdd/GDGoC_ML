91f87d91aaedbb8296af9ad9a0f97f854b9d3485
Evathrsprdd <156682779+Evathrsprdd@users.noreply.github.com>
2024-12-29 07:49:12 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 
 #  Load dataset (pastikan Anda mengganti 'salaries.csv' dengan path file Anda)
 def load_data():
